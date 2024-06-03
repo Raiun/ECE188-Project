6264a3359da82fc31405cc8eeb0dd7bb7d963e7c
Raiun <ryan.man821@gmail.com>
2024-06-03 12:05:33 -0700

    og version of audiomanager

--- ece-188/src/Components/AudioManager.tsx
+++ ece-188/src/Components/AudioManager.tsx
@@ -1,13 +1,11 @@
-"use client"
-
 import React, { useCallback, useEffect, useState } from "react";
 import axios from "axios";
-import Modal from "./Modal/Modal";
+import Modal from "./Modal/Modal"
 import AudioPlayer from "./AudioPlayer";
 import { TranscribeButton } from "./TranscribeButton";
 import Constants from "../utils/Constants";
-import { Transcriber } from "@/hooks/useTranscriber";
-//import Progress from "./Progress";
+import { Transcriber } from "../hooks/useTranscriber";
+import Progress from "./Progress";
 import AudioRecorder from "./AudioRecorder";
 
 function titleCase(str: string) {
@@ -24,6 +22,104 @@ function titleCase(str: string) {
 // https://github.com/openai/whisper/blob/248b6cb124225dd263bb9bd32d060b6517e067f8/whisper/tokenizer.py#L79
 const LANGUAGES = {
     en: "english",
+    zh: "chinese",
+    de: "german",
+    es: "spanish/castilian",
+    ru: "russian",
+    ko: "korean",
+    fr: "french",
+    ja: "japanese",
+    pt: "portuguese",
+    tr: "turkish",
+    pl: "polish",
+    ca: "catalan/valencian",
+    nl: "dutch/flemish",
+    ar: "arabic",
+    sv: "swedish",
+    it: "italian",
+    id: "indonesian",
+    hi: "hindi",
+    fi: "finnish",
+    vi: "vietnamese",
+    he: "hebrew",
+    uk: "ukrainian",
+    el: "greek",
+    ms: "malay",
+    cs: "czech",
+    ro: "romanian/moldavian/moldovan",
+    da: "danish",
+    hu: "hungarian",
+    ta: "tamil",
+    no: "norwegian",
+    th: "thai",
+    ur: "urdu",
+    hr: "croatian",
+    bg: "bulgarian",
+    lt: "lithuanian",
+    la: "latin",
+    mi: "maori",
+    ml: "malayalam",
+    cy: "welsh",
+    sk: "slovak",
+    te: "telugu",
+    fa: "persian",
+    lv: "latvian",
+    bn: "bengali",
+    sr: "serbian",
+    az: "azerbaijani",
+    sl: "slovenian",
+    kn: "kannada",
+    et: "estonian",
+    mk: "macedonian",
+    br: "breton",
+    eu: "basque",
+    is: "icelandic",
+    hy: "armenian",
+    ne: "nepali",
+    mn: "mongolian",
+    bs: "bosnian",
+    kk: "kazakh",
+    sq: "albanian",
+    sw: "swahili",
+    gl: "galician",
+    mr: "marathi",
+    pa: "punjabi/panjabi",
+    si: "sinhala/sinhalese",
+    km: "khmer",
+    sn: "shona",
+    yo: "yoruba",
+    so: "somali",
+    af: "afrikaans",
+    oc: "occitan",
+    ka: "georgian",
+    be: "belarusian",
+    tg: "tajik",
+    sd: "sindhi",
+    gu: "gujarati",
+    am: "amharic",
+    yi: "yiddish",
+    lo: "lao",
+    uz: "uzbek",
+    fo: "faroese",
+    ht: "haitian creole/haitian",
+    ps: "pashto/pushto",
+    tk: "turkmen",
+    nn: "nynorsk",
+    mt: "maltese",
+    sa: "sanskrit",
+    lb: "luxembourgish/letzeburgesch",
+    my: "myanmar/burmese",
+    bo: "tibetan",
+    tl: "tagalog",
+    mg: "malagasy",
+    as: "assamese",
+    tt: "tatar",
+    haw: "hawaiian",
+    ln: "lingala",
+    ha: "hausa",
+    ba: "bashkir",
+    jw: "javanese",
+    su: "sundanese",
 };
 
 export enum AudioSource {
@@ -33,7 +129,7 @@ export enum AudioSource {
 }
 
 export function AudioManager(props: { transcriber: Transcriber }) {
-    //const [progress, setProgress] = useState<number | undefined>(undefined);
+    const [progress, setProgress] = useState<number | undefined>(undefined);
     const [audioData, setAudioData] = useState<
         | {
               buffer: AudioBuffer;
@@ -47,16 +143,39 @@ export function AudioManager(props: { transcriber: Transcriber }) {
         string | undefined
     >(undefined);
 
+    const isAudioLoading = progress !== undefined;
+
     const resetAudio = () => {
         setAudioData(undefined);
         setAudioDownloadUrl(undefined);
     };
 
+    const setAudioFromDownload = async (
+        data: ArrayBuffer,
+        mimeType: string,
+    ) => {
+        const audioCTX = new AudioContext({
+            sampleRate: Constants.SAMPLING_RATE,
+        });
+        const blobUrl = URL.createObjectURL(
+            new Blob([data], { type: "audio/*" }),
+        );
+        const decoded = await audioCTX.decodeAudioData(data);
+        setAudioData({
+            buffer: decoded,
+            url: blobUrl,
+            source: AudioSource.URL,
+            mimeType: mimeType,
+        });
+    };
+
     const setAudioFromRecording = async (data: Blob) => {
         resetAudio();
+        setProgress(0);
         const blobUrl = URL.createObjectURL(data);
         const fileReader = new FileReader();
         fileReader.onprogress = (event) => {
+            setProgress(event.loaded / event.total || 0);
         };
         fileReader.onloadend = async () => {
             const audioCTX = new AudioContext({
@@ -64,6 +183,7 @@ export function AudioManager(props: { transcriber: Transcriber }) {
             });
             const arrayBuffer = fileReader.result as ArrayBuffer;
             const decoded = await audioCTX.decodeAudioData(arrayBuffer);
+            setProgress(undefined);
             setAudioData({
                 buffer: decoded,
                 url: blobUrl,
@@ -74,12 +194,77 @@ export function AudioManager(props: { transcriber: Transcriber }) {
         fileReader.readAsArrayBuffer(data);
     };
 
+    const downloadAudioFromUrl = async (
+        requestAbortController: AbortController,
+    ) => {
+        if (audioDownloadUrl) {
+            try {
+                setAudioData(undefined);
+                setProgress(0);
+                const { data, headers } = (await axios.get(audioDownloadUrl, {
+                    signal: requestAbortController.signal,
+                    responseType: "arraybuffer",
+                    onDownloadProgress(progressEvent) {
+                        setProgress(progressEvent.progress || 0);
+                    },
+                })) as {
+                    data: ArrayBuffer;
+                    headers: { "content-type": string };
+                };
+
+                let mimeType = headers["content-type"];
+                if (!mimeType || mimeType === "audio/wave") {
+                    mimeType = "audio/wav";
+                }
+                setAudioFromDownload(data, mimeType);
+            } catch (error) {
+                console.log("Request failed or aborted", error);
+            } finally {
+                setProgress(undefined);
+            }
+        }
+    };
+
+    // When URL changes, download audio
+    useEffect(() => {
+        if (audioDownloadUrl) {
+            const requestAbortController = new AbortController();
+            downloadAudioFromUrl(requestAbortController);
+            return () => {
+                requestAbortController.abort();
+            };
+        }
+    }, [audioDownloadUrl]);
+
     return (
         <>
-            <div className='w-32 flex flex-col justify-center items-center rounded-lg bg-white shadow-xl shadow-black/5 ring-2 ring-slate-700/10'>
+            <div className='flex flex-col justify-center items-center rounded-lg bg-white shadow-xl shadow-black/5 ring-1 ring-slate-700/10'>
                 <div className='flex flex-row space-x-2 py-2 w-full px-2'>
+                    <UrlTile
+                        icon={<AnchorIcon />}
+                        text={"From URL"}
+                        onUrlUpdate={(e) => {
+                            props.transcriber.onInputChange();
+                            setAudioDownloadUrl(e);
+                        }}
+                    />
+                    <VerticalBar />
+                    <FileTile
+                        icon={<FolderIcon />}
+                        text={"From file"}
+                        onFileUpdate={(decoded, blobUrl, mimeType) => {
+                            props.transcriber.onInputChange();
+                            setAudioData({
+                                buffer: decoded,
+                                url: blobUrl,
+                                source: AudioSource.FILE,
+                                mimeType: mimeType,
+                            });
+                        }}
+                    />
                     {navigator.mediaDevices && (
                         <>
+                            <VerticalBar />
                             <RecordTile
                                 icon={<MicrophoneIcon />}
                                 text={"Record"}
@@ -91,6 +276,11 @@ export function AudioManager(props: { transcriber: Transcriber }) {
                         </>
                     )}
                 </div>
+                {
+                    <AudioDataBar
+                        progress={isAudioLoading ? progress : +!!audioData}
+                    />
+                }
             </div>
             {audioData && (
                 <>
@@ -115,6 +305,21 @@ export function AudioManager(props: { transcriber: Transcriber }) {
                             icon={<SettingsIcon />}
                         />
                     </div>
+                    {props.transcriber.progressItems.length > 0 && (
+                        <div className='relative z-10 p-4 w-full'>
+                            <label>
+                                Loading model files... (only run once)
+                            </label>
+                            {props.transcriber.progressItems.map((data) => (
+                                <div key={data.file}>
+                                    <Progress
+                                        text={data.file}
+                                        percentage={data.progress}
+                                    />
+                                </div>
+                            ))}
+                        </div>
+                    )}
                 </>
             )}
         </>
@@ -288,6 +493,80 @@ function VerticalBar() {
     return <div className='w-[1px] bg-slate-200'></div>;
 }
 
+function AudioDataBar(props: { progress: number }) {
+    return <ProgressBar progress={`${Math.round(props.progress * 100)}%`} />;
+}
+
+function ProgressBar(props: { progress: string }) {
+    return (
+        <div className='w-full bg-gray-200 rounded-full h-1 dark:bg-gray-700'>
+            <div
+                className='bg-blue-600 h-1 rounded-full transition-all duration-100'
+                style={{ width: props.progress }}
+            ></div>
+        </div>
+    );
+}
+
+function UrlTile(props: {
+    icon: JSX.Element;
+    text: string;
+    onUrlUpdate: (url: string) => void;
+}) {
+    const [showModal, setShowModal] = useState(false);
+
+    const onClick = () => {
+        setShowModal(true);
+    };
+
+    const onClose = () => {
+        setShowModal(false);
+    };
+
+    const onSubmit = (url: string) => {
+        props.onUrlUpdate(url);
+        onClose();
+    };
+
+    return (
+        <>
+            <Tile icon={props.icon} text={props.text} onClick={onClick} />
+            <UrlModal show={showModal} onSubmit={onSubmit} onClose={onClose} />
+        </>
+    );
+}
+
+function UrlModal(props: {
+    show: boolean;
+    onSubmit: (url: string) => void;
+    onClose: () => void;
+}) {
+    const [url, setUrl] = useState(Constants.DEFAULT_AUDIO_URL);
+
+    const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setUrl(event.target.value);
+    };
+
+    const onSubmit = () => {
+        props.onSubmit(url);
+    };
+
+    return (
+        <Modal
+            show={props.show}
+            title={"From URL"}
+            content={
+                <>
+                    {"Enter the URL of the audio file you want to load."}
+                </>
+            }
+            onClose={props.onClose}
+            submitText={"Load"}
+            onSubmit={onSubmit}
+        />
+    );
+}
+
 function FileTile(props: {
     icon: JSX.Element;
     text: string;
@@ -431,6 +710,42 @@ function Tile(props: {
                 </div>
             )}
         </button>
+    );
+}
+
+function AnchorIcon() {
+    return (
+        <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth='1.5'
+            stroke='currentColor'
+        >
+            <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244'
+            />
+        </svg>
+    );
+}
+
+function FolderIcon() {
+    return (
+        <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth='1.5'
+            stroke='currentColor'
+        >
+            <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M3.75 9.776c.112-.017.227-.026.344-.026h15.812c.117 0 .232.009.344.026m-16.5 0a2.25 2.25 0 00-1.883 2.542l.857 6a2.25 2.25 0 002.227 1.932H19.05a2.25 2.25 0 002.227-1.932l.857-6a2.25 2.25 0 00-1.883-2.542m-16.5 0V6A2.25 2.25 0 016 3.75h3.879a1.5 1.5 0 011.06.44l2.122 2.12a1.5 1.5 0 001.06.44H18A2.25 2.25 0 0120.25 9v.776'
+            />
+        </svg>
     );
 }
 
